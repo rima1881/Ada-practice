900062a9a97c9299ca0afb0eeb2081ba3ef3b468
amir <amd.gharibipour@outlook.com>
2023-07-27 15:30:28 -0700

    coen/1/part4 initial

---
+++ coen243/1/part4/common/part4.adb
@@ -0,0 +1,20 @@
+with Ada.Text_IO;use Ada.Text_IO;
+with Ada.Integer_Text_IO;use Ada.Integer_Text_IO;
+
+procedure part4  is
+    num : Integer;
+    i : Integer;
+begin
+    
+
+    Put("Enter a number");
+    Get(num);
+
+    Put(num rem 5);
+
+    --while i < num/2 loop
+
+
+   -- end loop;
+
+end part4;
